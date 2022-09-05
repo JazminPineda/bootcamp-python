0327a45f0d1426af9dc7a9d2f0cd6fab2ec0a6ef
Jazmin Pineda <jppinedac940@hotmail.com>
2022-09-05 18:16:25 -0300

    calcule el área de un triángulo, recibiendo la altura y la base como parámetros y otra función que calcule el área de un círculo recibiendo el radio del mismo

--- 4 Func/triangulo.py
+++ 4 Func/triangulo.py
@@ -15,15 +15,17 @@ def area_angulo_agudo( a,  angulo = 89,):
 
 def circulo():
     return lambda radio : math.pi * radio**2
-       
+
+        
 area_triangulo = areaTriangulo(4,3)
 triang_rectangulo = areaTriang_equilatero(4, 6)
 triang_Equilatero = area_angulo_agudo(5)
 radio_cir = circulo() #puntero de funcion 
-
+radio_lambda = lambda radio : math.pi * radio **2
 
 print(f"El área de un triangulo es {area_triangulo}, \n \
         El área de un triangulo Rectángulo es {triang_rectangulo}, \n \
         El área de un triangulo Equilátero es {triang_Equilatero}, \n \
         El área de un circulo  es {radio_cir.__call__(5)}" )
+print(f"Soy radio lambda aqui hablando {radio_lambda(5)}")
 
