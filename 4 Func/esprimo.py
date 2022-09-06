a4376095bd6735be0abed934255de65480d58be3
Jazmin Pineda <jppinedac940@hotmail.com>
2022-09-05 22:05:42 -0300

    Se instala librerias

--- 4 Func/esprimo.py
+++ 4 Func/esprimo.py
@@ -1,15 +1,11 @@
-from ast import Return
 import math
-import numpy as np 
-import seaborn as sns
-import pandas as pd
 
 def esprimo(num):
     #Partimos de una lista de números que van de  2  hasta un determinado número
     for i in range(2 , int(num/2)):
         #Eliminamos de la lista los múltiplos de  2.
         if num % i == 0:
-             resultado = "número compuesto"
+             resultado = "número compuesto/entero"
            
         else:
             resultado = "número primo"
@@ -24,17 +20,17 @@ def cuadrado(num):
 
 
 num1 = int(input("Ingresa un número "))
+num2 = num1 +1
 
-
+b = esprimo(num1)
+a = cuadrado(num2)
+print( f" {b} El número es primo? {a}")
 #O(n) and O(√n) Visually
-# def visually():
-#     n = 20
-#     x = np.arange(n)
-#     y1 = np.sqrt(x)
-#     y2 = x
-#     df = pd.DataFrame({"O(√n)":y1,"O(n)":y2})
-#     sns.set_theme()
-#     sns.lineplot(data = df)
 
-# print(esprimo(num1), f"El número es primo? {cuadrado(num1)}", )
-# print(visually())
+# n = 20
+# x = np.arange(n)
+# y1 = np.sqrt(x)
+# y2 = x
+# df = pd.DataFrame({"O(√n)":y1,"O(n)":y2})
+# sns.set_theme()
+# sns.lineplot(data = df)
